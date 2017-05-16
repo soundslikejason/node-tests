7ba74be0de3afc2257c24511c3a571cf19d1566d
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-15 20:36:22 -0700

    testing express applications pt.2

--- server/server.js
+++ server/server.js
@@ -3,7 +3,30 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+// GET /users
+// Give users a name prop and age prop
+app.get('/users', (req, res) => {
+  res.send([
+    {
+      name: 'Joey',
+      age: 32
+    },
+    {
+      name: 'Patricia',
+      age: 21
+    },
+    {
+      name: 'Sophia',
+      age: 18
+    }
+  ]);
 });
 
 app.listen(3000);
+module.exports.app = app;
---
+++ server/server.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const expect = require('expect');
+
+var app = require('./server').app;
+
+it('should return hello world response', (done) => {
+  request(app)
+    .get('/')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found.'
+      });
+    })
+    .end(done);
+});
+
+// Make a new test
+// assert 200
+// assert that 'Joey' exists in users array
+it('should return a user object', (done) => {
+  request(app)
+    .get('/users')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        name: 'Joey',
+        age: 32
+      });
+    })
+    .end(done);
+});
