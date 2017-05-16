4e988fc1faea9018a977e5e06ea86c3dbb024b25
Jason Leong <soundslikejason@users.noreply.github.com>
2017-05-16 09:19:43 -0700

    organizing test with describe()

--- server/server.test.js
+++ server/server.test.js
@@ -3,30 +3,38 @@ const expect = require('expect');
 
 var app = require('./server').app;
 
-it('should return hello world response', (done) => {
-  request(app)
-    .get('/')
-    .expect(404)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        error: 'Page not found.'
-      });
-    })
-    .end(done);
+describe('Server', () => {
+  
+  describe('GET /', () => {
+    it('should return hello world response', (done) => {
+      request(app)
+      .get('/')
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          error: 'Page not found.'
+        });
+      })
+      .end(done);
+    });
+  });
+  
+  describe('GET /users', () => {
+    // Make a new test
+    // assert 200
+    // assert that 'Joey' exists in users array
+    it('should return a user object', (done) => {
+      request(app)
+      .get('/users')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body).toInclude({
+          name: 'Joey',
+          age: 32
+        });
+      })
+      .end(done);
+    });
+  });
 });
 
-// Make a new test
-// assert 200
-// assert that 'Joey' exists in users array
-it('should return a user object', (done) => {
-  request(app)
-    .get('/users')
-    .expect(200)
-    .expect((res) => {
-      expect(res.body).toInclude({
-        name: 'Joey',
-        age: 32
-      });
-    })
-    .end(done);
-});
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,36 +1,41 @@
 const expect = require('expect');
 const utils = require('./utils');
 
-it('should add two numbers', () => {
-  var res = utils.add(33, 11);
+describe('Utils', () => {
   
-  expect(res).toBe(44).toBeA('number');
-  // if (res !== 44) {
-  //   throw new Error(`Expected 44, but got ${res}.`);
-  // }
-});
-
-it('should async add two numbers', (done) => {
-  utils.asyncAdd(4, 3, (sum) => {
-    expect(sum).toBe(7).toBeA('number');
-    done();
+  describe('#add', () => {
+    it('should add two numbers', () => {
+      var res = utils.add(33, 11);
+      
+      expect(res).toBe(44).toBeA('number');
+      // if (res !== 44) {
+      //   throw new Error(`Expected 44, but got ${res}.`);
+      // }
+    });
   });
-});
-
-
-it('should square a number', () => {
-  var res = utils.square(8);
   
-  expect(res).toBe(64).toBeA('number');
-  // if (res !== 64) {
-  //   throw new Error(`Expected 64, but got ${res}.`);
-  // }
-});
-
-it('should async square a number', (done) => {
-  utils.asyncSquare(5, (square) => {
-    expect(square).toBe(25).toBeA('number');
-    done();
+  it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+      expect(sum).toBe(7).toBeA('number');
+      done();
+    });
+  });
+  
+  
+  it('should square a number', () => {
+    var res = utils.square(8);
+    
+    expect(res).toBe(64).toBeA('number');
+    // if (res !== 64) {
+    //   throw new Error(`Expected 64, but got ${res}.`);
+    // }
+  });
+  
+  it('should async square a number', (done) => {
+    utils.asyncSquare(5, (square) => {
+      expect(square).toBe(25).toBeA('number');
+      done();
+    });
   });
 });
 
